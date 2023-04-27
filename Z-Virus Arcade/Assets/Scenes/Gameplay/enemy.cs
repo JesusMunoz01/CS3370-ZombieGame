ae7e2b84188c7144fb11cb9bd8a8fdd4fda6b46f
Jesus <jesus.munoz01@utrgv.edu>
2023-04-26 20:09:41 -0500

    weapons scripts

--- Z-Virus Arcade/Assets/Scenes/Gameplay/enemy.cs
+++ Z-Virus Arcade/Assets/Scenes/Gameplay/enemy.cs
@@ -74,7 +74,7 @@ public class enemy : MonoBehaviour
                 }
             }
         }
-        else{}
+        else{Destroy(gameObject);}
 
     }
 
---
+++ Z-Virus Arcade/Assets/Scenes/Playtest/WeaponSwitch.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    public Weapons weapon;
+    public int currentWeapon = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UseWeapon();
+        weapon = GetComponentInChildren<Weapons>();
+        Debug.Log(weapon.range);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int prevWeapon = currentWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+                currentWeapon = 0;
+            else
+                currentWeapon++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if(currentWeapon <= 0)
+                currentWeapon = transform.childCount - 1;
+            else
+                currentWeapon--;
+        }
+
+        if (prevWeapon != currentWeapon)
+        {
+            UseWeapon();
+        }
+    }
+
+    void UseWeapon()
+    {
+        int index = 0;
+        foreach(Transform weapon in transform)
+        {
+            if(index == currentWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+
+            index++;
+        }
+        weapon = GetComponentInChildren<Weapons>();
+    }
+
+    public int getDamage()
+    {
+        return weapon.damage;
+    }
+
+    public float getRange()
+    {
+        return weapon.range;
+    }
+}
