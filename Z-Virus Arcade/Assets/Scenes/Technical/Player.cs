d44b92da3d797ed27611455ca69311e7c5b5a5b6
Jesus <jesus.munoz01@utrgv.edu>
2023-05-11 00:57:02 -0500

    animations

--- Z-Virus Arcade/Assets/Scenes/Technical/Player.cs
+++ Z-Virus Arcade/Assets/Scenes/Technical/Player.cs
@@ -22,6 +22,7 @@ public class Player : MonoBehaviour
     public bool searchPressed = false;
     public int health = 100;
     bool animPlaying = false;
+    bool playerDead = false;
     float animTimer = 1f;
 
     void Start()
@@ -32,119 +33,140 @@ public class Player : MonoBehaviour
         weapon = GetComponentInChildren<WeaponSwitch>();
         healthBar.MaxHealth(health);
         test = gameObject.GetComponent<Animator>();
-        test.Play("Idle_Normal_SwordAndShield 0");
+        test.Play("Idle_Battle_SwordAndShield");
     }
 
     void Update()
     {
-        if (animPlaying = false){
-            test.Play("Idle_Normal_SwordAndShield 0");
-        }
-        else {animTimer -= Time.deltaTime;}
+        // if (animPlaying == false){
+        //     test.Play("Idle_Normal_SwordAndShield 0");
+        // }
+        // else {animTimer -= Time.deltaTime;}
 
-        if(animTimer <= 0){
-            animPlaying = false;
-        }
+        // if(animTimer <= 0){
+        //     animPlaying = false;
+        // }
         // Character Movement
-        if(m_cc.isGrounded){
-            m_vel = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
-            //test.Play("MoveFWD_Battle_RM_SwordAndShield");
-            m_vel = m_vel * 10.0f;
+        if(health > 0){
+            if(m_cc.isGrounded){
+                m_vel = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+                m_vel = m_vel * 10.0f;
 
-            if(Input.GetAxis("Jump") > 0){
-                m_vel += m_vel + (Vector3.up * JUMP_SPEED);
+                if(Input.GetKey(KeyCode.W)){
+                    test.Play("MoveFWD_Battle_RM_SwordAndShield");
+                }
+                else if(Input.GetKey(KeyCode.A)){
+                    test.Play("MoveLFT_Battle_RM_SwordAndShield");
+                }
+                else if(Input.GetKey(KeyCode.S)){
+                    test.Play("MoveBWD_Battle_RM_SwordAndShield");
+                }
+                else if(Input.GetKey(KeyCode.D)){
+                    test.Play("MoveRGT_Battle_RM_SwordAndShield");
+                }
+                else
+                    test.Play("Idle_Battle_SwordAndShield");
+
+                if(Input.GetAxis("Jump") > 0){
+                    m_vel += m_vel + (Vector3.up * JUMP_SPEED);
+                    test.Play("JumpFull_Normal_InPlace_SwordAndShield");
+                }
+
+            }else{
+                    //test.Play("Idle_Normal_SwordAndShield 0");
+                    m_vel += (Vector3.down * GRAVITY * Time.deltaTime);
             }
 
-        }else{
-                test.Play("Idle_Normal_SwordAndShield 0");
-                 m_vel += (Vector3.down * GRAVITY * Time.deltaTime);
-        }
+            // Camera rotation with Q and E
+            if(Input.GetKey(KeyCode.Q)){
+                transform.Rotate(Vector3.down * 100 * Time.deltaTime);
+            }
+            if(Input.GetKey(KeyCode.E)){
+                transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+            }
 
-        // Camera rotation with Q and E
-        if(Input.GetKey(KeyCode.Q)){
-            transform.Rotate(Vector3.down * 100 * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.E)){
-            transform.Rotate(Vector3.up * 100 * Time.deltaTime);
-        }
+            // Camera rotation with mouse
+            transform.Rotate(transform.up, Input.GetAxis("Mouse X") * 3, 0);
 
-        // Camera rotation with mouse
-        transform.Rotate(transform.up, Input.GetAxis("Mouse X") * 3, 0);
+            // Stop searching
+            if(Input.GetKey(KeyCode.G)){
+                Ray ray = view.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
-        // Stop searching
-        if(Input.GetKey(KeyCode.G)){
-            Ray ray = view.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+                if(Physics.Raycast(ray, out hit, 100)){
+                        RemoveFocus();
+                    }
+                }
 
-            if(Physics.Raycast(ray, out hit, 100)){
+            // Searching function
+            if(Input.GetKeyDown(KeyCode.F)){
+                Ray ray = view.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit, 100)){
+                    Interactable search = hit.collider.GetComponent<Interactable>();
+                    if (search != null){
+                        searchPressed = true;
+                        SetFocus(search);
+                    }
+                }
+
+            }
+
+            // Attack & Calculations
+            if(Input.GetMouseButtonDown(0)){
+                Ray ray = view.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if(Physics.Raycast(ray, out hit, weapon.getRange())){
+                    enemy entity = hit.collider.GetComponent<enemy>();
+                    if (entity != null){
+                        animPlaying = true;
+                        animTimer = 0.5f;
+                        test.Play("Attack02_SwordAndShiled");
+                        SetEnemy(entity);
+                    }
+                }
+            }
+            
+            // Lock mouse
+            if(Input.GetMouseButtonDown(1)){
+                if(Cursor.lockState == CursorLockMode.Locked)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+            }
+            
+            // Searching counter
+            if(searchPressed == true){
+                //searchTimer -= Time.deltaTime;
+                //Debug.Log(searchTimer);
+
+                // if(searchTimer <= 0){
+                //     searchPressed = false;
+                //     RemoveFocus();
+                //     searchTimer = 5f;
+                // } 
+                if(focus.Complete()){
+                    searchPressed = false;
                     RemoveFocus();
-                }
+                    searchTimer = 5f;
+                }     
             }
 
-        // Searching function
-        if(Input.GetKeyDown(KeyCode.F)){
-            Ray ray = view.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(Physics.Raycast(ray, out hit, 100)){
-                Interactable search = hit.collider.GetComponent<Interactable>();
-                if (search != null){
-                    searchPressed = true;
-                    SetFocus(search);
-                }
-            }
-
-        }
-
-        // Attack & Calculations
-        if(Input.GetMouseButtonDown(0)){
-            Ray ray = view.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(Physics.Raycast(ray, out hit, weapon.getRange())){
-                enemy entity = hit.collider.GetComponent<enemy>();
-                if (entity != null){
-                    animPlaying = true;
-                    animTimer = 0.5f;
-                    test.Play("Attack01_SwordAndShiled");
-                    test.Play("Idle_Normal_SwordAndShield 0");
-                    SetEnemy(entity);
-                }
+            // Self damage (Testing purposes)
+            if(Input.GetKeyDown(KeyCode.L)){
+                TakeDamage(weapon.getDamage());
             }
         }
-        
-        // Lock mouse
-        if(Input.GetMouseButtonDown(1)){
-            if(Cursor.lockState == CursorLockMode.Locked)
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }else
-            {
-                Cursor.lockState = CursorLockMode.Locked;
+        else
+            if(playerDead == false){
+            test.Play("Die01_SwordAndShield");
+            playerDead = true;
             }
-        }
-        
-        // Searching counter
-        if(searchPressed == true){
-            //searchTimer -= Time.deltaTime;
-            //Debug.Log(searchTimer);
-
-            // if(searchTimer <= 0){
-            //     searchPressed = false;
-            //     RemoveFocus();
-            //     searchTimer = 5f;
-            // } 
-            if(focus.Complete()){
-                searchPressed = false;
-                RemoveFocus();
-                searchTimer = 5f;
-            }     
-        }
-
-        // Self damage (Testing purposes)
-        if(Input.GetKeyDown(KeyCode.L)){
-            TakeDamage(weapon.getDamage());
-        }
     }
 
     void FixedUpdate()
